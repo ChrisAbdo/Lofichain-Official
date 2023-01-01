e8c0815d6ba89a9863f6349c6e06da9d7f66d9df
chris abdo <chris.j.abdo@gmail.com>
2022-12-31 22:42:00 -0500

    ADDED: Framer motion animation for landing page

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Lottie from 'react-lottie-player';
 import audio from '../public/audio.json';
 import Balancer from 'react-wrap-balancer';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
@@ -12,69 +13,74 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="hero">
-        <div className="hero-content flex-col lg:flex-row-reverse ">
-          <Lottie loop animationData={audio} play style={{ width: 300 }} />
-          <div>
-            <h1 className="text-7xl font-bold">
-              <span className="text-[#50e3c2]">Create.</span>
-              <br />
-              <span className="text-[#9013fe]">Listen.</span>
-              <br />
-              <span className="text-[#bd10e0]">Earn.</span>
-            </h1>
-            <p className="py-6 text-3xl max-w-md">
-              <Balancer>
-                LofiChain is a Web&#40;3&#41; radio that rewards users for
-                creating and listening
-              </Balancer>
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        <div className="hero">
+          <div className="hero-content flex-col lg:flex-row-reverse ">
+            <Lottie loop animationData={audio} play style={{ width: 300 }} />
+            <div>
+              <h1 className="text-7xl font-bold">
+                <span className="text-[#50e3c2]">Create.</span>
+                <br />
+                <span className="text-[#9013fe]">Listen.</span>
+                <br />
+                <span className="text-[#bd10e0]">Earn.</span>
+              </h1>
+              <p className="py-6 text-3xl max-w-md">
+                <Balancer>
+                  LofiChain is a Web&#40;3&#41; radio that rewards users for
+                  creating and listening
+                </Balancer>
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-        <a
-          href="https://nextjs.org/docs"
-          className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-        >
-          <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-          <p className="mt-4 text-xl">
-            Find in-depth information about Next.js features and its API.
-          </p>
-        </a>
+        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+          <a
+            href="https://nextjs.org/docs"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Find in-depth information about Next.js features and its API.
+            </p>
+          </a>
 
-        <a
-          href="https://nextjs.org/learn"
-          className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-        >
-          <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-          <p className="mt-4 text-xl">
-            Learn about Next.js in an interactive course with quizzes!
-          </p>
-        </a>
+          <a
+            href="https://nextjs.org/learn"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Learn about Next.js in an interactive course with quizzes!
+            </p>
+          </a>
 
-        <a
-          href="https://github.com/vercel/next.js/tree/canary/examples"
-          className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-        >
-          <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-          <p className="mt-4 text-xl">
-            Discover and deploy boilerplate example Next.js projects.
-          </p>
-        </a>
+          <a
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Discover and deploy boilerplate example Next.js projects.
+            </p>
+          </a>
 
-        <a
-          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-        >
-          <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-          <p className="mt-4 text-xl">
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </motion.div>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
