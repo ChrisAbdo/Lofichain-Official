d69073c2c73056dcde78cb8bb84d174b09907885
chris abdo <chris.j.abdo@gmail.com>
2022-12-31 22:09:23 -0500

    ADDED: Thirdweb SDK

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,22 @@
 import '../styles/globals.css';
 
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+
+import Navbar from '../components/Navbar';
+
+const activeChainId = ChainId.Goerli;
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ThirdwebProvider desiredChainId={activeChainId}>
+        <div className="bg-black">
+          <Navbar />
+          <Component {...pageProps} />
+        </div>
+      </ThirdwebProvider>
+    </>
+  );
 }
 
 export default MyApp;
