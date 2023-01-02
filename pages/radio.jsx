72108919d48e4cf86445c25a32cea43944f4b60c
chris abdo <chris.j.abdo@gmail.com>
2023-01-01 21:20:56 -0500

    ADDED: Trying to fix the SVGs

--- pages/radio.jsx
+++ pages/radio.jsx
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 import Rewind from '../icons/Rewind';
-import Play from '../icons/Play';
 import Forward from '../icons/Forward';
 
 import Eye from '../icons/Eye';
@@ -73,7 +72,20 @@ const Alert = () => {
                 <Rewind />
               </button>
               <button className="btn btn-primary">
-                <Play />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+                  />
+                </svg>
               </button>
               <button className="btn btn-primary">
                 <Forward />
