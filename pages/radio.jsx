5c9801cee77b48e2b30531c88b883a12141b19c6
chris abdo <chris.j.abdo@gmail.com>
2023-01-01 19:15:38 -0500

    ADDED: icons folder for all svgs to maintain readability. Working on music player card now

--- pages/radio.jsx
+++ pages/radio.jsx
@@ -2,6 +2,13 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
+import Rewind from '../icons/Rewind';
+import Play from '../icons/Play';
+import Forward from '../icons/Forward';
+
+import Eye from '../icons/Eye';
+import Eyeslash from '../icons/Eyeslash';
+
 const Alert = () => {
   const [isOpen, setIsOpen] = useState(true);
 
@@ -23,23 +30,10 @@ const Alert = () => {
                 </span>
               </div>
             </Link>
-            {/* <button onClick={() => setIsOpen(false)}>Close</button> */}
+
             <div className="flex-none">
               <button onClick={() => setIsOpen(false)} className="btn btn-sm">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                  />
-                </svg>
+                <Eye />
               </button>
             </div>
           </motion.div>
@@ -48,25 +42,7 @@ const Alert = () => {
             <div />
             <div className="flex-none">
               <button onClick={() => setIsOpen(true)} className="btn btn-sm">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
+                <Eyeslash />
               </button>
             </div>
           </div>
@@ -74,15 +50,27 @@ const Alert = () => {
       </AnimatePresence>
 
       <div className="hero p-12">
-        <div className="card lg:card-side bg-base-100 shadow-xl">
+        <div className="card lg:card-side border border-[#2a2a2a] ">
           <figure>
-            <img src="https://placeimg.com/400/400/arch" alt="Album" />
+            <img
+              src="https://placeimg.com/400/400/arch"
+              alt="."
+              className="p-6"
+            />
           </figure>
           <div className="card-body">
             <h2 className="card-title">New album is released!</h2>
             <p>Click the button to listen on Spotiwhy app.</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Listen</button>
+            <div className="card-actions justify-between">
+              <button className="btn btn-primary">
+                <Rewind />
+              </button>
+              <button className="btn btn-primary">
+                <Play />
+              </button>
+              <button className="btn btn-primary">
+                <Forward />
+              </button>
             </div>
           </div>
         </div>
