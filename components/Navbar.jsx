dc922e9bc817fbed82451fa050f6db61abecd45e
chris abdo <chris.j.abdo@gmail.com>
2023-01-01 22:06:06 -0500

    ADDED: Updates to navbar for responsive dropdown

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -23,35 +23,16 @@ const Navbar = () => {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 border-b border-r border-[#2a2a2a]"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-[#2a2a2a] rounded-box w-52 border-b border-r border-[#2a2a2a]"
           >
             <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <a className="justify-between">
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
+              <a href="/radio">Radio</a>
             </li>
             <li>
-              <a>Item 3</a>
+              <a href="/upload">Upload</a>
+            </li>
+            <li>
+              <a href="/profile">Profile</a>
             </li>
           </ul>
         </div>
